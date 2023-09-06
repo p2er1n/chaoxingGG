f97bb4c7be8a534298d84faa50a8bfa2ee8fd7c0
Peerin <1461200635@qq.com>
2023-09-06 14:15:06 +0800

    update features

--- chaoxingGG.js
+++ chaoxingGG.js
@@ -13,9 +13,13 @@ ui.layout(
 	</horizontal>
 	<horizontal>
 	<checkbox id="isMute" text="静音刷课"/>
+	<checkbox id="isReview" text="复习模式"/>
 	</horizontal>
-	<button id="start" text="开始刷课！" textSize="25sp"/>
+	<horizontal>
+	<button id="start" text="开始刷课" textSize="25sp"/>
 	<button id="stop" text="结束刷课" textSize="25sp"/>
+	<button id="checkUpdate" text="检查更新" textSize="25sp"/>
+	</horizontal>
 	<text id="status" text="脚本未运行" textColor="red"/>
 	<img id="ad"/>
 	</vertical>
@@ -29,17 +33,26 @@ let helpText = `1. 首先请确保已经开启无障碍功能！
 2. 请确保已经安装了超星学习通并且已经登陆账号。
 3. 请确保超星学习通目前处于退出状态或者停留在起始页面。
 4. 静音刷课功能需要允许程序修改系统权限。
-5. 按压音量上键可以停止刷课。`;
+5. 按压音量上键可以停止刷课。
+6. 蓝奏云文件夹访问密码：666
+7. 脚本执行过程中，除了按压音量上键停止刷课，不要手动干涉。`;
+
 let storage = storages.create("com.chaoxingGG.course");
 let lastCourse = storage.get("lastCourse");
 if(lastCourse != undefined){
     ui.course.setText(lastCourse);
 }
 let isMute = false;
+let isReview = false;
 let lastMute = storage.get("lastMute");
+let lastReview = storage.get("lastReview");
 if(lastMute != undefined){
     isMute = lastMute;
     ui.isMute.setChecked(isMute);
+}
+if(lastReview != undefined){
+    isReview = lastReview;
+    ui.isReview.setChecked(isReview);
 }
 let course = ui.course.text();
 let s;
@@ -51,6 +64,16 @@ ui.isMute.on("check", (checked)=>{
     else
 	isMute = false;
     storage.put("lastMute", isMute);
+});
+ui.isReview.on("check", (checked)=>{
+    if(checked)
+	isReview = true;
+    else
+	isReview = false;
+    storage.put("lastReview", isReview);
+});
+ui.checkUpdate.click(()=>{
+    app.openUrl("https://wwya.lanzoub.com/b04wheezc");
 });
 
 ui.start.click(()=>{
@@ -83,7 +106,7 @@ ui.start.click(()=>{
 	}
     }
     
-    s = engines.execScript("main", `let course = "${course}";` + "\nstart();\n" + start.toString());
+    s = engines.execScript("main", `let course = "${course}";let isReview = ${isReview};` + "\nstart();\n" + start.toString());
     setTimeout(function(){
 	if(s.getEngine().isDestroyed()){
 	    ui.status.setText("脚本未运行");
@@ -113,31 +136,110 @@ function start(){
 	if(click("跳过", 0)){
 	    toastLog("检测到开屏广告并跳过");
 	}
-	sleep(1000);
+	sleep(500);
     }
-    sleep(1000);
+    sleep(2000);
     
-    click("我的课程", 0);
-    sleep(1000);
+    for(let cnt = 1;cnt <= 10;++cnt){
+	if(click("我的课程", 0))
+	    break;
+	else{
+	    if(cnt != 10){
+		toastLog("点击我的课程失败"+cnt+"次");
+		sleep(2000);
+	    }else{
+		toastLog("无法进入我的课程，脚本退出");
+		sleep(2000);
+		return;
+	    }
+	}
+    }
+    sleep(2000);
 
-    click("我学的课", 0);
-    sleep(1000);
+    for(let cnt = 1;cnt <= 10;++cnt){
+	if(click("我学的课", 0))
+	    break;
+	else{
+	    if(cnt != 10){
+		toastLog("点击我学的课失败"+cnt+"次");
+		sleep(2000);
+	    }else{
+		toastLog("无法进入我学的课，脚本退出");
+		sleep(2000);
+		return;
+	    }
+	}
+    }
+    sleep(2000);
+    
+    for(let cnt = 1;cnt <= 10;++cnt){
+	if(click(course, 0))
+	    break;
+	else{
+	    if(cnt != 10){
+		toastLog("点击" + course + "失败" + cnt + "次");		
+		sleep(2000);
+	    }else{
+		toastLog("无法进入" + course + "，脚本退出");
+		sleep(2000);
+		return;
+	    }	    
+	}
+    }
+    sleep(2000);
 
-    click(course, 0);
-    sleep(1000);
-
-    click("章节", 0);
-    sleep(1000);
+    for(let cnt = 1;cnt <= 10;++cnt){
+	if(click("章节", 0))
+	    break;
+	else{
+	    if(cnt != 10){
+		toastLog("点击章节失败"+cnt+"次");
+		sleep(2000);
+	    }else{
+		toastLog("无法进入章节，脚本退出");
+		sleep(2000);
+		return;
+	    }
+	}
+    }
+    sleep(2000);
 
     //step into a task randomly
-    let icon = id("tv_icon").findOnce();
-    while(icon == null){
-	scrollDown(0);
-	sleep(1000);
-	icon = id("tv_icon").findOnce();
+    let taskIcon = null;
+    while(taskIcon === null){
+	let icons = id("tv_icon").find();
+	if(icons.empty()){
+	    toastLog("找不到任务点，脚本退出");
+	    sleep(2000);
+	    return;
+	}
+	for(let i = 0;i < icons.length;++i){
+	    let icon = icons[i];
+	    if(icon.text().length > 0 || isReview){
+		taskIcon = icon;
+		break;
+	    }
+	}
+	if(taskIcon == null){
+	    scrollDown(0);
+	    sleep(2000);	    
+	}
     }
-    click(icon.bounds().centerX() + 200, icon.bounds().centerY());
-    sleep(1000);
+    for(let cnt = 1;cnt <= 10;++cnt){
+	if(click(taskIcon.bounds().centerX() + 200, taskIcon.bounds().centerY()))
+	    break;
+	else{
+	    if(cnt != 10){
+		toastLog("进入任务点失败"+cnt+"次");	    
+		sleep(2000);
+	    }else{
+		toastLog("无法进入任务点，脚本退出");
+		sleep(2000);
+		return;
+	    }
+	}
+    }
+    sleep(2000);
 
     //for each page
     while(true){
@@ -151,21 +253,21 @@ function start(){
 		    return;
 		}
 		scrollDown(0);
-		sleep(1000);
+		sleep(2000);
 	    }
 	    text("下一节").findOnce().click();
-	    sleep(1000);
+	    sleep(2000);
 	    if(text("下一章").findOnce() != null){
 		text("下一章").findOnce().click();
-		sleep(1000);
+		sleep(2000);
 	    }
-	    sleep(1000);
+	    sleep(2000);
 	}else{
 	    //finish all the tasks.
 	    let completedPlayIcons = [];
-	    while(text("任务点").findOnce() != null){
+	    while(text("任务点").findOnce() != null || isReview){
 		let playIcons = text("播放").find();
-		sleep(1000);
+		sleep(2000);
 		//if there are some videos.
 		if(!playIcons.empty()){
 		    playIcons.forEach((playIcon)=>{
@@ -183,13 +285,18 @@ function start(){
 			if(hasPlayed)
 			    return;
 			playIcon.click();
-			sleep(1000);
+			sleep(2000);
 			while(true){
 			    //click some comfim popups automatically
 			    let widget = text("继续").findOnce();
 			    if( widget != null){
 				widget.click();
-				sleep(1000);
+				sleep(2000);
+			    }
+			    widget = text("重试").findOnce();
+			    if( widget != null){
+				widget.click();
+				sleep(2000);
 			    }
 			    let currentTimeLabel = id("land_current_time").findOnce();
 			    let totalTimeLabel = id("land_total_time").findOnce();
@@ -203,7 +310,7 @@ function start(){
 				toastLog("播放结束 -- "+ currentTime + "/" + totalTime);
 				sleep(2500);
 				back();
-				sleep(1000);
+				sleep(2000);
 				break;
 			    }
 			    sleep(5000);
@@ -212,7 +319,12 @@ function start(){
 		    });
 		}
 		scrollDown(0);
-		sleep(1000);
+		sleep(2000);
+
+		if(isReview){
+		    if(text("下一节").findOnce() != null || text("上一节").findOnce() != null)
+			break;
+		}
 	    }
 	    //check if it has reached to the bottom.
 	    while(text("下一节").findOnce() == null){
@@ -222,7 +334,7 @@ function start(){
 		    return;
 		}
 		scrollDown(0);
-		sleep(1000);
+		sleep(2000);
 	    }
 	    click("下一节", 0);
 	    sleep(2000);
